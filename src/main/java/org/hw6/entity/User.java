451abfe11a808cc5be53960df5103fd70b6708d8
vosooghifarshid95@gmail.com <vosooghifarshid95@gmail.com>
2022-07-23 10:23:48 +0430

    create User class

---
+++ src/main/java/org/hw6/entity/User.java
@@ -0,0 +1,52 @@
+package org.hw6.entity;
+
+
+import java.sql.Date;
+
+public class User {
+    private int id;
+    private String userName;
+    private String nationalCode;
+    private String birthday;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
