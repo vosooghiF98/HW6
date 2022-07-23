b194787ceb2e062c98ffd6e3bd4b78740baf5623
vosooghifarshid95@gmail.com <vosooghifarshid95@gmail.com>
2022-07-23 10:27:57 +0430

    create Article class

---
+++ src/main/java/org/hw6/entity/Article.java
@@ -0,0 +1,67 @@
+package org.hw6.entity;
+
+public class Article {
+    private int id;
+    private String title;
+    private String brief;
+    private String content;
+    private String createDate;
+    private boolean isPublished;
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBrief() {
+        return brief;
+    }
+
+    public void setBrief(String brief) {
+        this.brief = brief;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public boolean isPublished() {
+        return isPublished;
+    }
+
+    public void setPublished(boolean published) {
+        isPublished = published;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
