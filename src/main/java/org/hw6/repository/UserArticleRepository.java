a6c5ebb992fa6d98d4f1daa5eaf3a46a5355f95c
vosooghifarshid95@gmail.com <vosooghifarshid95@gmail.com>
2022-07-23 13:00:18 +0430

    create UserArticleRepository class

---
+++ src/main/java/org/hw6/repository/UserArticleRepository.java
@@ -0,0 +1,34 @@
+package org.hw6.repository;
+
+import org.hw6.config.DBConfig;
+import org.hw6.entity.Article;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserArticleRepository {
+    public ResultSet loadByUserId (int id) throws SQLException {
+        String query = """
+                select * from articles where userid = ?
+                """;
+        PreparedStatement preparedStatement = DBConfig.getConnection().prepareStatement(query);
+        preparedStatement.setInt(1,id);
+        return preparedStatement.executeQuery();
+    }
+
+    public void edit(Article article, int userId) throws SQLException {
+        String query = """
+                update articles set title = ? , brief = ? , content = ? , ispublished = ? where id = ? and userid = ?;
+                """;
+        PreparedStatement preparedStatement = DBConfig.getConnection().prepareStatement(query);
+        preparedStatement.setString(1,article.getTitle());
+        preparedStatement.setString(2, article.getBrief());
+        preparedStatement.setString(3,article.getContent());
+        preparedStatement.setBoolean(4,article.isPublished());
+        preparedStatement.setInt(5,article.getId());
+        preparedStatement.setInt(6,userId);
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+    }
+}
